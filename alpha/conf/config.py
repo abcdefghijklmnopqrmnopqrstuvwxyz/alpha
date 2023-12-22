be489f0329a15591ee87391253edab8568822a05
Erik Boháč <113086173+abcdefghijklmnopqrmnopqrstuvwxyz@users.noreply.github.com>
2023-12-22 21:07:26 +0100

    Delete alpha directory

--- alpha/conf/config.py
+++
@@ -1,6 +0,0 @@
-timetable_path = './conf/timetable.csv'
-subjects_path = './conf/subjects.json'
-timeout = 180
-offset = 2
-
-# !!! Do not change names of the variables, delete, or rename this file !!!
